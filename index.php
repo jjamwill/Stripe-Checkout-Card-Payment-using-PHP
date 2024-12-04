ea566687eeed8d01617428714c19deddf53d3bd9
jjamwill <38432944+jjamwill@users.noreply.github.com>
2024-12-04 01:53:48 -0500

    Create index.php
    Go to https://youtu.be/UjVDy2Z-Rok for a video explanation and demonstration

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>myShop | Stripe</title>
+    <meta charset="UTF-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container p-4">
+        <h5 class="mb-3">Adidas Manchester United Home Jersey 24/25</h5>
+
+        <div class="card mb-3" style="width: 18rem;">
+            <img src="img/ManU_Jersey.webp" class="card-img-top" alt="ManU_Jersey">
+            <div class="card-body">
+                <p class="card-text">Adidas Manchester United Home Jersey for the 24/25 season.</p>
+                <p class="card-text"><strong>JMD$12,102.12</strong></p>
+            </div>
+        </div>
+    
+        <form method="post" action="checkout.php">
+            <button class="btn btn-primary">Make Payment</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
