ee0bc1df47dba1fe9990e51feaddefc796f7d854
jjamwill <38432944+jjamwill@users.noreply.github.com>
2024-12-04 01:56:16 -0500

    Create success.php

---
+++ success.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>myShop | Stripe</title>
+    <meta charset="UTF-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container p-4">
+        <h5 class="mb-3">Thank you for your payment!</h5>
+
+        <div class="card" style="width: 24rem;">
+            <img src="img/payment-success.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Payment Information</h6>
+                <p class="card-text">Your have purchased the 24/25 Manchester United Home Jersey.</p>
+                <a href="index.php" class="btn btn-primary">Browse More</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
